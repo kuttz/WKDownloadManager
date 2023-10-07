2a54084fa146e79b0fd9121b6daf8e60b9eba4fe
kuttz <wardaddyone@gmail.com>
2023-10-07 13:16:23 +0530

    -- Example project

---
+++ Example/WebViewDownloaderExample/WebViewDownloaderExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewDownloaderExample
+//
+//  Created by Sreekuttan D on 07/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
